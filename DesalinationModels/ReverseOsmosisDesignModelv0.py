b527a884778db8b7f0a719bf1f688a7d9199924f
adam-a-a <aaa2269@columbia.edu>
2019-09-17 16:55:36 -0400

    Completed a simple, RO model w/ERD, assuming fixed load case.

--- DesalinationModels/ReverseOsmosisDesignModelv0.py
+++ DesalinationModels/ReverseOsmosisDesignModelv0.py
@@ -16,50 +16,67 @@ altered as power shifts.
 from numpy import array,cumprod,insert
 from numpy.matlib import repmat
 from math import ceil
+from warnings import warn
 
-class ReverseOsmosisDesignModel(object):
-    def __init__(self,
+#class RODesign:
+#
+#    def __init__(self,
 ###### (soon to be)JSON Inputs (Inputs available in GUI for user to modify)
-                # Fluid properties
-                Cf=5                # Feed TDS, g/L or parts per trillion
-                T=298.15            # Feedwater Temperature [Kelvin]
-                
-                
-                CP=1.1              # Concentration polarization factor
-                
-                #Pump and ERD Parameters
-                nERD=0.9            # Energy recovery device efficiency
-                
-                #RO Plant Design Specifications
-                nominal_daily_cap_tmp=50000
-                Nel1=6              #number elements per vessel in stage 1
-                R1=.65               #Desired overall recovery rate
+# Fluid properties
+Cf=32#,                # Feed TDS, g/L or parts per trillion
+T=298.15#,            # Feedwater Temperature [Kelvin]
 
-                # RO Membrane Property Inputs: 
-'''    preferably loaded in from a table of membrane types 
-       and associated properties. The user would choose a membrane from list or enter parameters
-       manually based on his/her own membrane datasheet observations'''
-                # Using default values here based on manufacturer's datasheet for seawater RO membrane element: SWC4B MAX
-                Qpnom1=27.3/24      #nominal permeate production per membrane element (m3/hr)
-                Am1=40.8            #membrane area per membrane element (m^2) 
-                Pmax1=82.7          #Max pressure of membrane element (bar)
-                Ptest1=55.2         #Applied test pressure for each mem element
-                Ctest1=32           #membrane manufacturer's test feed salinity (TDS) for each element (parts per thousand)
-                SR1=99.8            #rated salt rejection of each element (%)
-                Rt1=.1              #Test recovery rate for each element
-                Pdropmax=1          #maximum pressure drop per membrane element
-                ):
 
-if(T>343.15):
-    raise Exception("The temperature should be below 45 degrees Celsius to avoid membrane damage.")
+CP=1.1#,              # Concentration polarization factor
+
+#Pump and ERD Parameters
+nERD=0.9#,            # Energy recovery device efficiency
+nBP=0.85
+nHP=0.85
+nFP=0.85
+
+
+#RO Plant Design Specifications
+nominal_daily_cap_tmp=50000#,
+Nel1=6#,              #number elements per vessel in stage 1
+R1=.65#,               #Desired overall recovery rate
+
+# RO Membrane Property Inputs: 
+#load in from a table of membrane types w/properties or enter manually.
+# Using default values here based on manufacturer's datasheet for seawater RO membrane element: SWC4B MAX
+Qpnom1=27.3/24.0#,      #nominal permeate production per membrane element (m3/hr)
+Am1=40.8#,            #membrane area per membrane element (m^2) 
+Pmax1=82.7#,          #Max pressure of membrane element (bar)
+Ptest1=55.2#,         #Applied test pressure for each mem element
+Ctest1=32#,           #membrane manufacturer's test feed salinity (TDS) for each element (parts per thousand)
+SR1=99.8#,            #rated salt rejection of each element (%)
+Rt1=.1#,              #Test recovery rate for each element
+Pdropmax=1           #maximum pressure drop per membrane element (bar)
+Pfp=1
+#                ):
+
+   
+#                self.CP=CP
+#                self.nERD=nERD
+#                self.nominal_daily_cap_tmp=nominal_daily_cap_tmp
+#                self.Nel1=Nel1
+#                self.R1=R1
+#                self.Cf=Cf    
+#                
+#                    
+#                self.T=T
+#                
+#                if(self.T>343.15):
+#                    self.T=343.15
+#                    warn("The temperature should be below 45 degrees Celsius (343.15 K) to avoid membrane damage.")
 
 # Constants################################################################
 # Osmotic pressure calculated assuming the van't Hoff equation and NaCl
 vhfactor=2          # van't Hoff factor
-#######################################
+
 MW_nacl=58.443      # molecular weight of NaCl
 Ru=0.0831           # Universal Ideal Gas constant
-Rel=1/6;            #max element recovery rate based on manufacturer's recommended ratio of 5:1 for Qb:Qp
+Rel=1/6             #max element recovery rate based on manufacturer's recommended ratio of 5:1 for Qb:Qp
 
 #Intermediate Computations
 #Bs salt permeability
@@ -83,11 +100,26 @@ R1_max=sum(Rel*(i_nel))                         #max recovery for stage by summi
 NV1=ceil(nominal_daily_cap_tmp/Qpnom1/Nel1/24)  # Compute number of pressure vessels
 nominal_daily_cap=Qpnom1*Nel1*NV1*24            # Compute daily RO permeate production in m3/day
 
-Qp1temp=nominal_daily_cap/24
-NDP1=Qp1temp/(Nel1*NV1*Am1*A1)
+Qp1=nominal_daily_cap/24
+NDP1=Qp1/(Nel1*NV1*Am1*A1)
 Posm_f=vhfactor*Ru*T*CP/MW_nacl*Cf
 Posm_b=vhfactor*Ru*T*CP/MW_nacl*Cf/(1-R1)
-Pf1tmp=NDP1+CP*(Posm_f+Posm_b)*0.5 + Pd*0.5
+Pf1=NDP1+CP*(Posm_f+Posm_b)*0.5 + Pd*0.5
 #R1=1-vhfactor*Ru*T*CP/MW_nacl*Cf/(Pf1tmp-Pd-NDP1)
-Pb1=Pf1tmp-Pd; 
-Pbp_targ=Pf1tmp-nERD*Pb1
+Pb1=Pf1-Pd
+Pbp=Pf1-nERD*Pb1
+
+Qf1=Qp1/R1
+Qbp_targ=Qf1-Qp1
+Qbp=Qbp_targ
+Qhp=Qp1
+
+BP_power=Qbp*Pbp/nBP/36
+HP_power=Qhp*Pf1/nHP/36
+FP_power=Qf1*Pfp/nFP/36
+PowerTotal=FP_power+HP_power+BP_power
+PowerRO=HP_power+BP_power
+SEC=PowerTotal/Qp1
+SEC_RO=PowerRO/Qp1
+
+
