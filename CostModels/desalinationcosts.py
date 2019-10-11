19d0a89186a4810bc502e5aa659b30d36bed521f
adam-a-a <aaa2269@columbia.edu>
2019-10-11 12:40:00 -0400

    Adding comments to code

--- CostModels/desalinationcosts.py
+++ CostModels/desalinationcosts.py
@@ -7,41 +7,42 @@ SI Units
 @author: adamAatia
 """
 
-## Capital Recovery Factor 
+## Capital Recovery Factor function: compute capital recovery factor based on plant lifetime and discount rate. 
+# This capital recovery factor is multiplied by the total capex to compute the fixed charge capital cost.
 # t is time in years, r is discount or interest rate (fraction)
 def CR_factor(t=30,r=0.05): 
     return 1/((1 - (1 / (1 + r) ** t)) / r)
     
-daily_cap= 10000
+daily_cap= 10000         # daily capacity of desalination plant, m3/day
 
-unit_capex=1100 # USD/m3/day
+unit_capex=1100          # unit installed cost of desal plant, USD/m3/day
 
-specific_O_M=0.25
+specific_O_M=0.25        # specific O&M costs excluding energy cost, USD/m3 (per cubic meter of water produced)
 
-LCOE=0.01 #USD/kWh 
+LCOE=0.01                #levelized cost of electricity, USD/kWh 
 
-LCOH=0.0  #USD/kwhthermal
+LCOH=0.0                 #levelized cost of heat, USD/kwhthermal
 
-SEC_e=3 # kWh/m3 produced
+SEC_e=3                  # specific electrical energy consumption,  kWh/m3 produced
 
-SEC_th=20 #kWhthermal/m3
+SEC_th=20                # specific thermal energy consumption, kWhthermal/m3
 
-specific_energy_cost= SEC_e*LCOE + SEC_th*LCOH
+specific_energy_cost= SEC_e*LCOE + SEC_th*LCOH          #total specific energy cost, USD/m3
 
-utilization_factor=0.95
-
-
-total_annual_water=daily_cap*365*utilization_factor
+utilization_factor=0.95  #percent of year that desalination plant operates at full capacity (i.e., capacity factor), unitless
 
 
-total_capex=daily_cap*unit_capex
+total_annual_water=daily_cap*365*utilization_factor     #total annual water produced by desalination plant, m3
 
-lifetime=50
 
-discount_rate=0.05
+total_capex=daily_cap*unit_capex                        # total capital cost of desalination plant, USD
 
-annualized_capex=total_capex*CR_factor(lifetime,discount_rate)
+lifetime=50              # desalination plant lifetime, years        
 
-fixed_capex_per_m3=annualized_capex/total_annual_water
+discount_rate=0.05       #discount rate, unitless
 
-LCOW=fixed_capex_per_m3 + specific_O_M + specific_energy_cost
+annualized_capex=total_capex*CR_factor(lifetime,discount_rate) # annualized (fixed) capital cost, USD/year
+
+fixed_capex_per_m3=annualized_capex/total_annual_water      #fixed capital cost per cubic meter of water produced, USD/m3
+
+LCOW=fixed_capex_per_m3 + specific_O_M + specific_energy_cost   #levelized cost of water, USD/m3
