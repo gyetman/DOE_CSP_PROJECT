3c391da9c65a72c0b6bb59eb200735f87c79954b
adam-a-a <aaa2269@columbia.edu>
2019-11-13 10:24:15 -0500

    added comments

--- CostModels/desalinationcosts.py
+++ CostModels/desalinationcosts.py
@@ -12,23 +12,23 @@ SI Units
 def CR_factor(t=30,r=0.05): 
     return 1/((1 - (1 / (1 + r) ** t)) / r)
     
-daily_cap= 10000
+daily_cap= 100
 
-unit_capex=1100 # USD/m3/day
+unit_capex=800 # USD/m3/day
 
-specific_O_M=0.25
+specific_O_M=0.18 # total O&M cost per cubic meter
 
-LCOE=0.01 #USD/kWh 
+LCOE=0.05 #USD/kWh 
 
 LCOH=0.0  #USD/kwhthermal
 
-SEC_e=3 # kWh/m3 produced
+SEC_e=1 # Average specific electrical energy consumption, kWh/m3 produced
 
-SEC_th=20 #kWhthermal/m3
+SEC_th=25 # Average specific thermal energy consumption,kWhthermal/m3
 
 specific_energy_cost= SEC_e*LCOE + SEC_th*LCOH
 
-utilization_factor=0.95
+utilization_factor=.4
 
 
 total_annual_water=daily_cap*365*utilization_factor
@@ -36,7 +36,7 @@ total_annual_water=daily_cap*365*utilization_factor
 
 total_capex=daily_cap*unit_capex
 
-lifetime=50
+lifetime=30
 
 discount_rate=0.05
 
@@ -45,3 +45,5 @@ annualized_capex=total_capex*CR_factor(lifetime,discount_rate)
 fixed_capex_per_m3=annualized_capex/total_annual_water
 
 LCOW=fixed_capex_per_m3 + specific_O_M + specific_energy_cost
+
+print('\nTotal Capex=',total_capex,'\n\nAnnualized Capex=',annualized_capex,'\n\nLCOW=',LCOW)
