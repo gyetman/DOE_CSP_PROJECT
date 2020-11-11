433762b84eb8db752225be74c3f402185a621492
johnsquires <johnsquires73@gmail.com>
2020-11-11 18:27:20 -0500

    New functions to support parameter dependencies No longer WIP

--- app/apps/model_parameters.py
+++ app/apps/model_parameters.py
@@ -313,36 +313,9 @@ model_tables_layout = html.Div([parameters_navbar, tabs])
 #
 ### CALLBACKS
 #     
-
-#NOTE: EXAMPLE CALLBACK FOR CALLBACK DEPENDENCY - delete later
-# @app.callback(
-#     Output({'type':'solar-table', 'index': 'Power_CycleAvaialability_and_CurtailmentGeneral', 'model': 'tcslinear_fresnel'},'data'),
-#     [Input({'type':'solar-table', 'index': 'Power_CycleAvaialability_and_CurtailmentGeneral', 'model': 'tcslinear_fresnel'}, 'data_timestamp'),Input({'type':'solar-table', 'index': 'Power_CycleOperationGeneral', 'model': 'tcslinear_fresnel'}, 'data_timestamp')],
-#     [State({'type':'solar-table', 'index': 'Power_CycleAvaialability_and_CurtailmentGeneral', 'model': 'tcslinear_fresnel'}, 'data')],
-#     prevent_initial_call=True)
-# def upd_tbl(tblTime, tblTime2, tblData):
-#     inputs=dash.callback_context.inputs
-#     triggered=dash.callback_context.triggered
-#     print(tblData)
-#     for row in tblData:
-#         try:
-#             if row['Name'] == 'adjust:periods':
-#                 print('found row')
-#                 row['Value'] = str(tblData[helpers.index_in_list_of_dicts(tblData,'Name','adjust:hourly')]['Value'] + .001)
-#             else:
-#                 row['Value'] = float(row['Value']) ** 2
-#         except(ValueError):
-#             row['Value'] = 99
-#     print(f'states: {tblData}')
-#     print(f'type: {type(tblData)}')
-#     return tblData
-
-# NOTE: we'll want to eventually unpack the dict for the function call...
 for model, functions in pdeps.functions_per_model.items():
-    # output_ids=set()
-    # input_ids=set()
     model_type=pdeps.find_model_type(model)
-    for function in functions:     
+    for function in functions:
         @app.callback(
             [Output({'type': f'{model_type}-table',
                     'index': pdeps.table_indexes[model][outp],
@@ -358,26 +331,30 @@ for model, functions in pdeps.functions_per_model.items():
                     'index': pdeps.table_indexes[model][inp],
                     'model': model},
                     'data') 
-                for inp in function['inputs']],
+                for inp in function['inputs'] + function['outputs']],
             prevent_initial_call=True)
-        def get_table_outputs(inputs, states):
-            # for row in states:
-            #     try:
-            #         if row['Name'] == 'adjust:periods':
-            #             print('found row')
-            #             row['Value'] = str(states[helpers.index_in_list_of_dicts(states,'Name','adjust:hourly')]['Value'] + .001)
-            #         else:
-            #             row['Value'] = float(row['Value']) ** 2
-            #     except(ValueError):
-            #         row['Value'] = 99
-            return [states]
+        def get_table_outputs(*states):
+            '''
+            determines the function needed to update the output
+            table then sends the tables to that function using
+            function_switcher
+
+            states is a tuple that contains (in order):
+            input table timestamps,
+            input table data in list of dicts format,
+            output table data in list of dict format
+            '''
+            ctx = dash.callback_context
+            function_name = pdeps.get_function(context=ctx.outputs_list)
+            tables=[state for state in states if type(state)==list]
+            return pdeps.function_switcher(function_name,tables)
 
 @app.callback(
     Output('tabs-card', 'children'),
     [Input('tabs-data-initialize', 'children')]
 )
 def create_tabs_and_tables(x):
-    # return the tabs belonging to the collapse button
+    '''return the tabs belonging to the collapse button'''
 
     #create dict lookups for model and filenames
     app = helpers.json_load(cfg.app_json)
@@ -408,12 +385,6 @@ def create_tabs_and_tables(x):
     # find the weather file table and update
     if weather_file:
         wf_index = helpers.index_in_list_of_dicts(solar_model_vars,'Name','file_name')
-        #???
-        # NOT SURE IF THIS IS THE RIGHT INDEX
-        # wf_table = solar_model_vars[wf_index]
-        # wf_table[wf_index]['Value']=str(weather_file)
-        #SO INSTEAD OF UPDATING TABLE THAT NO LONGER EXIST WE 
-        #SHOULD PROBABLY JUST...
         solar_model_vars[wf_index]['Value']=str(weather_file)
 
     # find the TDS Feed Concentration table and update
@@ -569,9 +540,6 @@ def toggle_model_tabs(n1, n2, n3, is_open1, is_open2, is_open3):
     State({'type':'desal-table', 'index': ALL, 'model': ALL}, 'selected_row_ids'),
     State({'type':'finance-table', 'index': ALL, 'model': ALL}, 'selected_row_ids')],
     prevent_initial_call=True)
-    # For pulling the selected parametric variables???
-    #  Input('datatable-row-ids', 'selected_row_ids'),
-
 def update_model_variables_and_run_model(n_clicks, solTableData, desTableData, finTableData,
                                          selectedSolarRows, selectedDesalRows, selectedFinRows  ): 
     '''
@@ -594,8 +562,6 @@ def update_model_variables_and_run_model(n_clicks, solTableData, desTableData, f
         finance_output_vars = dict()
         parametric_info = dict()
 
-
-
         #NOTE need to transform the selectedXRows data to simple lists or sets
         # so that we can do a simple inclusion case  i.e. if x in y:
         # Collect selected variables from all tables into a single list
@@ -613,7 +579,6 @@ def update_model_variables_and_run_model(n_clicks, solTableData, desTableData, f
                 selected_fin.extend(fR)
                 
         # pull out variable names and values and add to new dict
-
         for solTable in solTableData:
             for sRow in solTable:
                 if sRow['id'] in selected_solar:
--- app/parameter_dependencies.py
+++ app/parameter_dependencies.py
@@ -2,10 +2,16 @@ import app_config as cfg
 import helpers
 
 # list parameter names that have input or output dependencies
+#NOTE: EACH TABLE CAN ONLY HAVE ONE CALLBACK OUTPUT!
+#NOTE: so create no more than one function per table in a model
 table_indexes = {
     'tcslinear_fresnel': 
         {'adjust:periods':'Power_CycleAvaialability_and_CurtailmentGeneral',
-         'adjust:hourly':'Power_CycleAvaialability_and_CurtailmentGeneral'}
+         'adjust:hourly':'Power_CycleAvaialability_and_CurtailmentGeneral'},
+    'linear_fresnel_dsg_iph':
+        {'nLoops': 'Solar_FieldSolar_Field_ParametersGeneral',
+         'q_pb_des':'System_DesignDesign_Point_ParametersHeat_Sink',
+         'I_bn_des':'System_DesignDesign_Point_ParametersSolar_Field'}
 }
 
 functions_per_model = {
@@ -16,21 +22,45 @@ functions_per_model = {
                 'inputs':['adjust:hourly'],
                 'function': 'adjuster'
             },
-            # {
-            #     'outputs':['adjust:hourly'],
-            #     'inputs':['adjust:hourly'],
-            #     'function': 'func2'
-            # },
+        ],
+    'linear_fresnel_dsg_iph':
+        [
+            {
+                'outputs': ['nLoops'],
+                'inputs': ['q_pb_des','I_bn_des'],
+                'function': 'IPH_LFDS_nloops'
+            },
         ]
 }
 
-#NOTE: EXAMPLE CALLBACK FROM model_parameters
-# @app.callback(
-#     Output({'type':'solar-table', 'index': 'Power_CycleAvaialability_and_CurtailmentGeneral', 'model': 'tcslinear_fresnel'},'data'),
-#     [Input({'type':'solar-table', 'index': 'Power_CycleAvaialability_and_CurtailmentGeneral', 'model': 'tcslinear_fresnel'}, 'data_timestamp'),Input({'type':'solar-table', 'index': 'Power_CycleOperationGeneral', 'model': 'tcslinear_fresnel'}, 'data_timestamp')],
-#     [State({'type':'solar-table', 'index': 'Power_CycleAvaialability_and_CurtailmentGeneral', 'model': 'tcslinear_fresnel'}, 'data')],
+#
+# SWITCHER FUNCTIONS
+#
+def adjuster(intables):
+    '''this is a sample function!'''
+    adjcon=get_value(intables,'adjust:constant')
+    adjper = str(float(adjcon)+.001)
+    update_val([intables[-1]],'adjust:periods',adjper)
+    return [intables[-1]]
 
+def IPH_LFDS_nloops(intables):
+    q_pb_des=get_value(intables,'q_pb_des')
+    I_bn_des=get_value(intables,'I_bn_des')
+    nloops = int(float(q_pb_des)/I_bn_des) + 1
+    update_val([intables[-1]],'nLoops',nloops)
+    return [intables[-1]]
 
+def function_switcher(func, intables):
+
+    dependent_var_switcher = {
+        'adjuster': adjuster,
+        'IPH_LFDS_nloops': IPH_LFDS_nloops
+    }
+    return dependent_var_switcher.get(func, KeyError("Function not found"))(intables)
+
+#
+#HELPER FUNCTIONS
+#
 def find_model_type(model):
     '''use the model name to determine what model type it is'''
     model_type={0:'solar',1:'desal',2:'finance'}
@@ -40,79 +70,55 @@ def find_model_type(model):
             return model_type[index]
     return None
 
-# #NOTE: we'll want to eventually unpack the dict for the function call...
-# #BELIEVE THIS, CODE WILL GO IN model_parameters.py
-# for model, functions in functions_per_model.items():
-#     model_type=find_model_type(model)
-#     for function in functions:
-#         @app.callback(
-#             Output[{'type': f'{model_type}-table',
-#                     'index': table_indexes[model[outp]],
-#                     'model': model}] 
-#                 for outp in function['outputs'],
-#             Input[{'type': f'{model_type}-table',
-#                     'index': table_indexes[model[inp]],
-#                     'model': model}] 
-#                 for inp in function['inputs'],
-#             State[{'type': f'{model_type}-table',
-#                     'index': table_indexes[model[inp]],
-#                     'model': model}] 
-#                 for inp in function['inputs'])
-#             def get_table_outputs(inputs, states):
-#                 print(f'inputs: {inputs}')
-#                 print(f'outputs: {outputs}')
-#                 return states[0]
+def get_function(context):
+    '''use callback context to determine the appropriate function name'''
+    #get the model name and table id from input context
+    model_name = context[0]['id']['model']
+    tblid=context[0]['id']['index']
+    #use configurations to determine the proper function
+    for f in functions_per_model[model_name]:
+        for var in f['outputs']:
+            if table_indexes[model_name][var]==tblid:
+                return f['function']
+    return None
 
 
-
-# for model, functions in functions_per_model.items():
-#     model_type=find_model_type(model)
-#     for function in functions:
-#         print(function)
-#         print(f'{[table_indexes[model][output] for output in function["outputs"]]}')
-
-#NOTE: EXAMPLE CODE FOR CALLBACK GENERATOR
-# def generate_output_callback(datasource_1_value, datasource_2_value):
-#     def output_callback(control_1_value, control_2_value):
-#         # This function can display different outputs depending on
-#         # the values of the dynamic controls
-#         return '''
-#             You have selected {} and {} which were
-#             generated from {} (datasource 1) and and {} (datasource 2)
-#         '''.format(
-#             control_1_value,
-#             control_2_value,
-#             datasource_1_value,
-#             datasource_2_value
-#         )
-#     return output_callback
-
-
-
-# def get_table_outputs(outvar, *invars):
-#     def x(invars):
-#         pass
-        
-#     dependent_var_switcher = {
-#         'var 1': x(invars)
-#     }
-# return dependent_var_switcher.get(outvar, "Variable not registered")
-
-
-#NOTE: MOVING & REWRITING 
-def register_tables(pdict):
+def get_table_id(varname, model_type, model):
     '''
     pull out the tab section and subsection that belongs to the 
     dependent variable, create the table ID and write the information
     to the dependencies json
     '''
-    dvdict = dict()
-    dp = pdict['id']
-    dvdict[dp] = f"{pdict['Tab']}{pdict['Section']}{pdict['Subsection']}".replace(' ','_').replace('(','').replace(')','').replace('/','').replace('000General','General')
-    # update json file
-    try:
-        helpers.json_update(data=dvdict, filename=cfg.dependencies_json)
-    except FileNotFoundError:
-        helpers.initialize_json(data=dvdict, filename=cfg.dependencies_json)
+    flkup = cfg.build_file_lookup(model,model,model)
+    jfile = flkup[f'{model_type}_variables_file']
+    with open(jfile, "r") as read_file:
+        json_vars_load = helpers.json.load(read_file)
+    index=helpers.index_in_list_of_dicts(json_vars_load[model],'Name',varname)
+    jvar=json_vars_load[model][index]
+    tab=jvar.get('Tab','General')
+    sec=jvar.get('Section','General')
+    subsec=jvar.get('Subsection','General')
+    return f"{tab}{sec}{subsec}".replace(' ','_').replace('(','').replace(')','').replace('/','')
+    
+def get_value(tables, name):
+    '''
+    looks for 'Name' key in tables(list of lists of dicts) and returns 'Value' value
+    '''
+    ls,index=helpers.index_in_lists_of_dicts(tables,'Name',name)
+    return ls[index]['Value']
+
+def update_val(tables,name,value):
+    '''
+    @tables: list of lists of dicts
+        NOTE: should only contain output dicts 
+    @name: string
+        key value to search containing value
+    @value: ???
+        value to search that matches the key,
+        this value will be updated 
+    '''
+    ls,index=helpers.index_in_lists_of_dicts(tables,'Name',name)
+    ls[index]['Value']=value
+
 
 
