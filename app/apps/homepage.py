c0665ff431f07692de9a0daa5bc401cc430bdd45
johnsquires <johnsquires73@gmail.com>
2021-03-10 17:03:57 -0500

    images working better inside assets directory

--- app/apps/homepage.py
+++ app/apps/homepage.py
@@ -5,8 +5,8 @@ import dash_html_components as html
 import dash_trich_components as dtc
 from dash.dependencies import Input, Output, State
 
-import app_config as cfg
-sys.path.insert(0,str(cfg.base_path))
+# import app_config as cfg
+# sys.path.insert(0,str(cfg.base_path))
 
 import helpers
 from app import app
@@ -19,51 +19,56 @@ from app import app
 
 card_documentation = dtc.Card(
     link='https://sam.nrel.gov/images/web_page_files/sam-help-2020-2-29-r1.pdf',
-    image='/images/documentation.png',
+    image='assets/images/documentation.png',
     title='Documentation',
     description="SAM documenation until we add our own.",
     badges=['Badge 1', 'Badge 2', 'Badge 3'],
+    style={'padding':16}
 )
 
 card_geospatial_resources = dtc.Card(
     link='https://resources-for-solar-desalination-columbia.hub.arcgis.com/',
-    image='/images/geospatial_resources.png',
+    image='assets/images/geospatial_resources.png',
     title='Geospatial Resources',
     description="About our resources HERE",
     badges=['Badge 1', 'Badge 2', 'Badge 3'],
+    style={'padding':16}
 )
 
 card_select_model = html.A(dtc.Card(
-    image='/images/model_selection.png',
+    image='/assets/images/model_selection.png',
     title='Select Model',
     description="Some quick example text to build on the card title and make up the bulk of the card's content.",
     badges=['Badge 1', 'Badge 2', 'Badge 3'],
-    git='https://githublinkhere.com'
+    git='https://githublinkhere.com',
+    style={'padding':16}
 ),href='/model-selection',style={'text-decoration': 'none'})
 
 card_select_site = html.A(dtc.Card(
-    image='/images/site_selection.png',
+    image='/assets/images/site_selection.png',
     title='Select Site',
     description="Some quick example text to build on the card title and make up the bulk of the card's content.",
     badges=['Badge 1', 'Badge 2', 'Badge 3'],
-    git='https://githublinkhere.com'
+    git='https://githublinkhere.com',
+    style={'padding':16}
 ),href='/dynamic-map',style={'text-decoration': 'none'})
 
 card_quick_analysis = dtc.Card(
     link='https://columbia.maps.arcgis.com/home/item.html?id=719c769587604bb9a2e098fa052ef1b9#overview',
-    image='/images/quick_analysis.png',
+    image='/assets/images/quick_analysis.png',
     title='Quick Analysis',
     description="Describe Quick Analysis here...",
     badges=['Badge 1', 'Badge 2', 'Badge 3'],
+    style={'padding':16}
 )
 
-cards = dbc.CardDeck([
+cards = dbc.Row([
     card_select_model,
     card_select_site,
     card_quick_analysis,
     card_geospatial_resources,
     card_documentation
-])
+],style={'padding':16})
 
 chart_navbar = dbc.NavbarSimple(
     # children=[dbc.NavItem(dbc.NavLink("Charts", href='/chart-results')),
@@ -83,6 +88,11 @@ homepage_layout = html.Div([
     html.H3("Solar Desalination Analysis Tool", className='text-success', style={'text-align':'center'}),
     dbc.Container([
         cards,
+        # card_select_model,
+        # card_select_site,
+        # card_quick_analysis,
+        # card_geospatial_resources,
+        # card_documentation,
         dbc.Button(
             html.Span(["Button  ", html.I(className="fas fa-plus-circle ml-2")])
         ),
