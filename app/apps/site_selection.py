99e5ca19872e016db0a222abf720e4fa7f05be3c
johnsquires <johnsquires73@gmail.com>
2021-05-04 18:45:36 -0400

    updated register_map and info_hover removed register_map function so callbacks run outside of it. imported app to do this
    updated hover_info so that desal and power plant hovers work. Also added functionality for clusters.

--- app/apps/site_selection.py
+++ app/apps/site_selection.py
@@ -13,16 +13,9 @@ from dash.dependencies import ALL, Input, Output, State, MATCH
 from dash.exceptions import PreventUpdate
 from pathlib import Path
 
-gis_data = app_config.gis_data_path
-# Div for legend
-# Site Selection
+from app import app
 
-# TODO:
-# add link (lines) and symbols for closest features
-# add legend(s)
-# 2. use a map-config file to load data, file locations, etc. 
-# 4. transfer & adapt code to write out parameters as json
-# 5. style to be consistent with menu interface
+gis_data = app_config.gis_data_path
 
 # Mapbox setup
 mapbox_url = "https://api.mapbox.com/styles/v1/{id}/tiles/{{z}}/{{x}}/{{y}}{{r}}?access_token={access_token}"
@@ -49,18 +42,6 @@ def get_style(feature):
     return dict()
 def get_d_style(feature):
     return dict(fillColor='orange', weight=2, opacity=1, color='white')
-
-def get_info(feature=None):
-    header = [html.H4("Feature Details")]
-    if feature:
-        if 'Technology' in list(feature[0]["properties"].keys()):
-            units = 'm3/day'
-        else:
-            units = 'MW'
-        return header + [html.B(feature[0]["properties"]["name"]), html.Br(),
-            f"{float(feature[0]['properties']['capacity_mw']):,.1f} Capacity {units}"]
-    else:
-        return header + ["Hover over a feature"]
 
 # load power plants JSON
 power_plants = dl.GeoJSON(
@@ -105,7 +86,7 @@ map_navbar = dbc.NavbarSimple(
     color='primary',
     dark=True,
     sticky='top',
-    style={'margin-bottom':20}
+    # style={'margin-bottom':20}
 )
 
 legend = html.Img(
@@ -161,7 +142,7 @@ radios = dbc.FormGroup([
 def render_map():
     return [
         map_navbar,
-        #dbc.Input(id='price_factor',value='1.0',type='hidden'),
+        # dbc.Input(id='price_factor',value='1.0',type='hidden'),
         dbc.Row([
             dbc.Col([
                 site_selection_map,
@@ -175,7 +156,7 @@ def render_map():
                 html.H3('Site details:', className='text-success'),
                 html.Div(id=SITE_DETAILS2)
             ],width=3)
-        ],style={'padding':20})
+        ],style={'padding':40})
     ]
 
 
@@ -186,91 +167,112 @@ theme_ids = {
     'regulatory': regulatory
 }
 
-def register_map(app):
-    @app.callback(Output(BASE_LAYER_ID, "url"),
-                [Input(BASE_LAYER_DROPDOWN_ID, "value")])
-    def set_baselayer(url):
-        return url
 
-    @app.callback(Output('theme-layer','children'),
-                 [Input('theme-dropdown', 'value')])
-    def set_theme_layer(theme):
-        return theme_ids[theme]
+@app.callback(Output(BASE_LAYER_ID, "url"),
+            [Input(BASE_LAYER_DROPDOWN_ID, "value")])
+def set_baselayer(url):
+    return url
 
-    @app.callback(Output(USER_POINT, 'position'),
-                  [Input(MAP_ID, 'click_lat_lng')])
-    def click_coord(coords):
-    # '''
-    # Callback for updating the map after a user click 
-    # TODO: in addition to returning 0,0, update icon
-    # to be "invisible".
-    # '''
-        if not coords:
-            return [0,0]
+@app.callback(Output('theme-layer','children'),
+                [Input('theme-dropdown', 'value')])
+def set_theme_layer(theme):
+    return theme_ids[theme]
+
+@app.callback(Output(USER_POINT, 'position'),
+                [Input(MAP_ID, 'click_lat_lng')])
+def click_coord(coords):
+# '''
+# Callback for updating the map after a user click 
+# TODO: in addition to returning 0,0, update icon
+# to be "invisible".
+# '''
+    if not coords:
+        return [0,0]
+    else:
+        return coords
+
+@app.callback([Output(SITE_DETAILS2, 'children'),Output("closest-facilities", 'children')],
+                [Input(MAP_ID, 'click_lat_lng')],prevent_initial_call=True)
+def get_point_info(lat_lng):
+    ''' callback to update the site information based on the user selected point'''
+    print('in callback for clicking')
+    if lat_lng is None:
+        return('Click on the Map to see site details.'), [0,0]
+    else:
+        markdown = dcc.Markdown(str(pointLocationLookup.lookupLocation(lat_lng)))
+        closest = pointLocationLookup.getClosestInfrastructure(lat_lng)
+        if not closest:
+            return markdown, [None,None,None,None]
+        elif 'plant' in closest.keys():
+            desal = dl.Polyline(positions=[lat_lng,closest['desal']], color='#FF0000', children=[dl.Tooltip("Desal Plant")])          
+            plant = dl.Polyline(positions=[lat_lng,closest['plant']], color='#ffa500', children=[dl.Tooltip("Power Plant")])
+            canal = dl.Polyline(positions=[lat_lng,closest['canal']], color='#add8e6', children=[dl.Tooltip("Canal/Piped Water")])
+            water = dl.Polyline(positions=[lat_lng,closest['water']], color='#000000', children=[dl.Tooltip("Water Network Proxy")])
+            return markdown, [desal,plant,canal,water]
+        elif 'desal' in closest.keys():
+            desal = dl.Polyline(positions=[lat_lng,closest['desal']], color='#FF0000', children=[dl.Tooltip("Desal Plant")])
+            return markdown, [desal,None,None,None]
         else:
-            return coords
+            return markdown, [None,None,None,None]
 
-    @app.callback([Output(SITE_DETAILS2, 'children'),Output("closest-facilities", 'children')],
-                    [Input(MAP_ID, 'click_lat_lng')],prevent_initial_call=True)
-    def get_point_info(lat_lng):
-        ''' callback to update the site information based on the user selected point'''
-        print('in callback for clicking')
-        if lat_lng is None:
-            return('Click on the Map to see site details.'), [0,0]
-        else:
-            markdown = dcc.Markdown(str(pointLocationLookup.lookupLocation(lat_lng)))
-            closest = pointLocationLookup.getClosestInfrastructure(lat_lng)
-            if not closest:
-                return markdown, [None,None,None,None]
-            elif 'plant' in closest.keys():
-                desal = dl.Polyline(positions=[lat_lng,closest['desal']], color='#FF0000', children=[dl.Tooltip("Desal Plant")])          
-                plant = dl.Polyline(positions=[lat_lng,closest['plant']], color='#ffa500', children=[dl.Tooltip("Power Plant")])
-                canal = dl.Polyline(positions=[lat_lng,closest['canal']], color='#add8e6', children=[dl.Tooltip("Canal/Piped Water")])
-                water = dl.Polyline(positions=[lat_lng,closest['water']], color='#000000', children=[dl.Tooltip("Water Network Proxy")])
-                return markdown, [desal,plant,canal,water]
-            elif 'desal' in closest.keys():
-                desal = dl.Polyline(positions=[lat_lng,closest['desal']], color='#FF0000', children=[dl.Tooltip("Desal Plant")])
-                return markdown, [desal,None,None,None]
-            else:
-                return markdown, [None,None,None,None]
-
-            
-    @app.callback(
-        Output(component_id='next-button',component_property='children'),
-        [Input(component_id=SITE_DETAILS2,component_property='children')],
-        [State(MAP_ID,'children')],
-        prevent_initial_call=True
-    )
-    def enableButton(site,site_properties):
-        ''' output to enable next button after a site has been selected '''
-        if site == 'Click on the Map to see site details.':
-            raise PreventUpdate
-        else:
-            return(
-                html.Div([
-                    html.Div(id='button-div'),
-                    dbc.Button('Select Models', color="primary",
-                            href='http://127.0.0.1:8077/model-selection'), 
-                ], className='row',
-                ) 
-            )
-
-
-    @app.callback(Output('info', 'children'),
-            [Input({'type':'json_theme', 'index': ALL}, 'hover_feature')]
+        
+@app.callback(
+    Output(component_id='next-button',component_property='children'),
+    [Input(component_id=SITE_DETAILS2,component_property='children')],
+    [State(MAP_ID,'children')],
+    prevent_initial_call=True
+)
+def enableButton(site,site_properties):
+    ''' output to enable next button after a site has been selected '''
+    if site == 'Click on the Map to see site details.':
+        raise PreventUpdate
+    else:
+        return(
+            html.Div([
+                html.Div(id='button-div'),
+                dbc.Button('Select Models', color="primary",
+                        href='http://127.0.0.1:8077/model-selection'), 
+            ], className='row',
+            ) 
         )
-    def info_hover(feature):
-        ''' callback for feature hover '''
-        return(get_info(feature))
+
+
+@app.callback(Output('info', 'children'),
+        [Input({'type':'json_theme', 'index': ALL}, 'hover_feature')]
+    )
+def info_hover(features):
+    ''' callback for feature hover '''
+    header = [html.H4("Feature Details")]
+    #feature is a list of dicts, grab first feature
+    feature = features[0]
+    if feature:
+        #check if feature is a cluster
+        if feature['properties']['cluster']:
+            print(f'CLUSTER')
+            return header + ["Click cluster to expand"]
+        #if feature is Desalination Plant
+        elif 'Technology' in feature['properties'].keys():
+            name = feature['properties']['Project name']
+            capacity_field = feature['properties']['Capacity (m3/d)']
+            units = 'm3/day'
+            return header+[html.B(name), html.Br(),
+                f"{capacity_field} Capacity {units}"]
+        #feature is Power Plant
+        else:
+            name = feature['properties']['name']
+            capacity_field = feature['properties']['capacity_mw']
+            units = 'MW'
+            return header + [html.B(name), html.Br(),
+                f"{float(capacity_field):,.1f} Capacity {units}"]
+    else:
+        return header + ["Hover over a feature"]
 
 
 external_stylesheets = [dbc.themes.FLATLY]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = 'Site Selection (Beta)'
-app.layout = html.Div(
-    render_map()
-)
-register_map(app)
+
+# register_map(app)
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8150)
