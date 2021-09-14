47592a99455f0deb3ad12ef8556f7759d21c8f1e
Greg Yetman <gyetman@gmail.com>
2021-09-14 12:46:02 -0400

    global weather file creation

--- utils/create_global_weather_json_from_files.py
+++ utils/create_global_weather_json_from_files.py
@@ -1,38 +1,47 @@
 import glob
 import json
+import fiona
+# import app_config as cfg
 from pathlib import Path
-search = r'/Users/gyetman/DOE/DOE_CSP_PROJECT/SAM_flatJSON/solar_resource/*.csv'
+from importlib.machinery import SourceFileLoader
 
-HEADER = {
-    "type": "FeatureCollection",
-    "name": "global_weather_file",
-    "crs": { "type": "name","properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" }}
-}
+SEARCH_PATTERN = r'../SAM_flatJSON/solar_resource/*.csv'
 
-flist = glob.glob(search)
+# read the schema from the existing weather index file, 
+# which is in the GISQUERY folder. This should be read from
+# the config file, but hard coded here. 
 
-print(f'{len(flist)} csv files')
+with fiona.open('../GISQueryData/global_weather_file.geojson') as source:
+    out_schema = source.schema
+    print(out_schema['properties'])
 
-with open('/Users/gyetman/DOE/DOE_CSP_PROJECT/GISQueryData/global_weather_file.geojson', 'r') as f:
-    data = json.load(f)
 
-file_names = set()
-for record in data['features']:
-    file_names.add(record['properties']['filename'])
 
-print(len(file_names))
+wlist = glob.glob(SEARCH_PATTERN)
 
-new_file_names = set()
-for f in flist:
-    new_file_names.add(Path(f).name)
+print(f'{len(wlist)} csv files')
 
-removed = file_names - new_file_names
+rows = []
 
-for record in data['features']:
-    if record['properties']['filename'] in removed:
-        data['features'].remove(record)
+for wfile in wlist:
+    meta = {}
+    with open(wfile, 'r') as f:
+        line = f.readline()
+        for val in line.split(','):
+            meta[val.strip()] = ''
+        line = f.readline()
+        #print(line)
+        i = 0
+        for item in meta:
+            meta[item] = line.split(',')[i].strip()
+            i+=1
+        meta['filename'] = Path(wfile).name
+        rows.append(meta)
 
-with open('./new_weather_file.geojson', 'w') as f:
-    f.write(json.dumps(data))
+print(rows[0])
+print(out_schema['properties'])
+print(set(out_schema['prope']) - set(rows[0].keys()))
+# with open('./new_weather_file.geojson', 'w') as f:
+#     f.write(json.dumps(data))
 
 print('...done')
