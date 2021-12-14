b2936635a997b5505f587b577291cf1f43e03822
Zhuoran Zhang <zz2322@columbia.edu>
2021-12-14 13:26:18 -0500

    Update pages and matlab file

--- app/app_config.py
+++ app/app_config.py
@@ -117,7 +117,7 @@ ADD = "/assets/docs/Detailed description for Input Variables.pdf"
 
 Documentation = {
     #Desal
-    'FO':{'doc':SDAT,'page':27}, 'VAGMD':{'doc':SDAT,'page':27}, 'MDB':{'doc':SDAT,'page':27}, 'LTMED':{'doc':SDAT,'page':27}, 'ABS':{'doc':SDAT,'page':27}, 'MEDTVC':{'doc':SDAT,'page':27}, 'NUN':{'doc':SDAT,'page':27}, 'RO':{'doc':SDAT,'page':27}, 'COMRO':{'doc':SDAT,'page':27}, 'OARO':{'doc':SDAT,'page':27}, 'LSRRO':{'doc':SDAT,'page':27}, 'RO_FO':{'doc':SDAT,'page':27}, 'RO_MDB':{'doc':SDAT,'page':27}, 'Generic':{'doc':SDAT,'page':27},
+    'FO':{'doc':SDAT,'page':35}, 'VAGMD':{'doc':SDAT,'page':27}, 'MDB':{'doc':SDAT,'page':32}, 'LTMED':{'doc':SDAT,'page':21}, 'ABS':{'doc':SDAT,'page':27}, 'MEDTVC':{'doc':SDAT,'page':27}, 'NUN':{'doc':SDAT,'page':27}, 'RO':{'doc':SDAT,'page':40}, 'COMRO':{'doc':SDAT,'page':27}, 'OARO':{'doc':SDAT,'page':40}, 'LSRRO':{'doc':SDAT,'page':27}, 'RO_FO':{'doc':SDAT,'page':42}, 'RO_MDB':{'doc':SDAT,'page':42}, 'Generic':{'doc':SDAT,'page':44},
     #Financial
     'utilityrate5':{'doc':SAMD,'page':771}, 'lcoefcr':{'doc':SAMD,'page':829}, 'iph_to_lcoefcr':{'doc':SAMD,'page':829}, 'levpartflip':{'doc':SAMD,'page':803}, 'equpartflip':{'doc':SAMD,'page':813}, 'saleleaseback':{'doc':SAMD,'page':821}, 'singleowner':{'doc':SAMD,'page':787},
     #Solar
